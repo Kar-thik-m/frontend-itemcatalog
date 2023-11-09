0d257be6a9825f673bc5a7999ac57f1a56401583
kar-thik-m <sparrowkarthik007@gmail.com>
2023-11-09 16:54:09 +0530

    c

--- src/pages/LogedPage.jsx
+++ src/pages/LogedPage.jsx
@@ -1,22 +1,56 @@
-import { useNavigate } from "react-router-dom"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import loadstyle from '../pages/Loadpage.module.css';
+import frontimg from "../assets/front.jpg";
+import { Piechart } from '../chart/Piechart';
 
-const LogedPage = () => {
-  const navigate=useNavigate()
-  return (
-    <section className="container-fluid" style={{marginTop:'11%'}}>
-    <section className="row justify-content-center">
-      <section className="col-12 col-sm-6 col-md-4">
-    <h1>your are loged in successfully</h1>
-    <button
-    className="btn btn-danger btn-block ml-4"
-    onClick={()=>{localStorage.removeItem('user');
-  navigate('/login')}}
-    >Logout</button>
-       </section>
-    </section>
-  </section>
+const App = () => {
+    const Navigate = useNavigate();
+    return (
+        <div>
+            <nav className={loadstyle.nav}>
+            
+         
+                <div ><i class="fa fa-ravelry" aria-hidden="true" style={{fontSize:"0.8cm",color:"white"}}><b style={{marginLeft:"10px"}}>ITEM CATALOG</b></i></div>
+                <button className={loadstyle.link} onClick={() => window.location.href = "/nav"}>HOME</button>
+                <button className={loadstyle.link} onClick={() => window.location.href = "/add"}>ADD ITEMS</button>
+                <button className={loadstyle.link} onClick={() => window.location.href = "/furnitures"}>FURNITURES</button>
+                <button className={loadstyle.link} onClick={() => window.location.href = "/electronics"}>ELECTRONICS</button>
+                <button className={loadstyle.link} onClick={() => window.location.href = "/fashion"}>FASHION</button>
+                <button className={loadstyle.buttons} onClick={() => {
+                    localStorage.removeItem('user');
+                    Navigate('/login')
+                }}
+                >Logout</button>
+            </nav>
+            <div className={loadstyle.res}>
 
-  )
-}
+                <div class="card bg-dark text-white">
+                    <img style={{ width: "100%", height: "auto" }} src={frontimg} class="card-img" alt="..." />
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">DATA PERCENTAGE</h5>
+                        <p class="card-text" style={{ width: "50%", height: "auto" }}>{<Piechart />}</p>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+                <div style={{ width: "100%", height: "auto", backgroundColor: "rgb(59, 84, 105)",color:"#e4e0e0",textAlign:"center" }}>
+                    <h5>ABOUT</h5>
+                    <ul style={{textAlign:'center',fontFamily:"monospace"}}>
+                        <li>Contact Us</li>
+                        <li>Aout us</li>
+                        <li>carees</li>
+                        <li>Corporate Information</li>
 
-export default LogedPage
+                    </ul>
+
+                    <i class="fa fa-vimeo" aria-hidden="true"></i>
+                    <i class="fa fa-instagram" aria-hidden="true" style={{marginLeft:"10px"}}></i>
+                    <i class="fa fa-youtube" aria-hidden="true" style={{marginLeft:"10px"}}></i>
+                    <i class="fa fa-facebook" aria-hidden="true"  style={{marginLeft:"10px"}}></i>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
